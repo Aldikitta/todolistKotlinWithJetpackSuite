c1352d8f539aa9436fbfd47eee2fb96996645cbd
Aldikitta <andi.kitta77@gmail.com>
2022-05-05 22:22:31 +0800

    implementing datastore

--- app/src/main/java/com/aldikitta/modernmvvmtodohiltflowroomjetpack/ui/tasks/TaskFragment.kt
+++ app/src/main/java/com/aldikitta/modernmvvmtodohiltflowroomjetpack/ui/tasks/TaskFragment.kt
@@ -8,12 +8,16 @@ import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aldikitta.modernmvvmtodohiltflowroomjetpack.R
+import com.aldikitta.modernmvvmtodohiltflowroomjetpack.data.SortOrder
 import com.aldikitta.modernmvvmtodohiltflowroomjetpack.databinding.FragmentTasksBinding
 import com.aldikitta.modernmvvmtodohiltflowroomjetpack.util.onQueryTextChanged
 
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TaskFragment : Fragment(R.layout.fragment_tasks) {
@@ -50,21 +54,26 @@ class TaskFragment : Fragment(R.layout.fragment_tasks) {
         searchView.onQueryTextChanged {
             viewModel.searchQuery.value = it
         }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.actionHideCompletedTasks).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.actionSortByName -> {
-
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
                 true
             }
             R.id.actionSortByDateCreated -> {
-
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
                 true
             }
             R.id.actionHideCompletedTasks -> {
                 item.isChecked = !item.isChecked
-
+                viewModel.onHideCompletedClick(item.isChecked)
                 true
             }
             R.id.actionDeleteAllCompletedTasks -> {
--- app/src/main/java/com/aldikitta/modernmvvmtodohiltflowroomjetpack/ui/tasks/TaskViewModel.kt
+++ app/src/main/java/com/aldikitta/modernmvvmtodohiltflowroomjetpack/ui/tasks/TaskViewModel.kt
@@ -2,23 +2,45 @@ package com.aldikitta.modernmvvmtodohiltflowroomjetpack.ui.tasks
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.aldikitta.modernmvvmtodohiltflowroomjetpack.data.PreferencesManager
+import com.aldikitta.modernmvvmtodohiltflowroomjetpack.data.SortOrder
 import com.aldikitta.modernmvvmtodohiltflowroomjetpack.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TaskViewModel @Inject constructor(
-    private val taskDao: TaskDao
+    private val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
     val searchQuery = MutableStateFlow("")
 
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    val preferencesFlow = preferencesManager.preferencesFlow
+//
+//    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+//    val hideCompleted = MutableStateFlow(false)
+
+    private val tasksFlow =
+        combine(searchQuery, preferencesFlow) { query, filterPreferences ->
+            Pair(query, filterPreferences)
+        }.flatMapLatest {(query, filterPreferences) ->
+            taskDao.getTasks(query, filterPreferences.sortOrder, filterPreferences.hideCompleted)
+        }
+
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompletedClick(hideCompleted: Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
     }
 
     val tasks = tasksFlow.asLiveData()
 }
+
